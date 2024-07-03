b33dd713273a87880fddbf71bbe0c43819ee87bb
Abdelrahman Tarek <abdelrahman.tarek.omar@gmail.com>
2024-07-04 00:37:09 +0300

    task one

---
+++ task1.js
@@ -0,0 +1,25 @@
+// for the following code write what should be the output for each code snippet
+
+var x = 4;
+function changeXValue() {
+  x++;
+}
+changeXValue();
+console.log(x);
+
+for (var x = 0; x < 3; x++) {}
+
+console.log(x);
+
+// ==========================================
+
+let y = 4;
+function changeYValue() {
+  y++;
+}
+changeYValue();
+console.log(y);
+
+for (let y = 0; y < 3; y++) {}
+
+console.log(y);
