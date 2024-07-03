54e3ee085a15587f41bbbc58c3fffafbddc07fa8
Abdelrahman Tarek <abdelrahman.tarek.omar@gmail.com>
2024-07-04 00:46:46 +0300

    task four

---
+++ task4.js
@@ -0,0 +1,10 @@
+const users = [
+  { id: 1, name: "ahmed", age: 22 },
+  { id: 2, name: "ali", age: 23 },
+  { id: 3, name: "hussein", age: 24 },
+];
+// write a function that only return the user object of id 2
+
+// without using array methods
+
+// using array methods
