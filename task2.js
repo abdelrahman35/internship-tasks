c919c92f9ee01ae34c38488eb73ead19e6d0011f
Abdelrahman Tarek <abdelrahman.tarek.omar@gmail.com>
2024-07-04 00:42:42 +0300

    task two

---
+++ task2.js
@@ -0,0 +1,23 @@
+// write a function that return the following
+
+const exampleArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// 1. returns even numbers from the example array
+
+//  without using array methods
+
+//  using array methods
+
+// ===================================================================
+// 2. return the odd numbers from the example array multiplied with a multiplier
+
+//  without using array methods
+
+//  using array methods
+
+// ===================================================================
+// 3. return sum of all numbers of the example array
+
+//  without using array methods
+
+//  using array methods
